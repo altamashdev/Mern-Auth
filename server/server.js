ad72cb05a29a7223fdfdce47eee243d59d635ad5
Coder <altamasma064@gmail.com>
2025-11-25 23:42:17 +0530

    all fixx

--- server/server.js
+++ server/server.js
@@ -1,43 +1,37 @@
-    // Don's Forget To add extension with any import file
-    import express from "express";
-    import cors from "cors";
-    import "dotenv/config";
-    import cookieParser from "cookie-parser";
-    import connectDB from "./config/mongodb.js";
-    import authRouter from "./routes/authRoutes.js";
-    import userRouter from "./routes/userRoutes.js";
+// Don's Forget To add extension with any import file
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
 
-    const app = express();
-    const port = process.env.PORT || 4000;
-    connectDB();
+// in this variable we can use all frontend url where we want to add this backend
+const allowedOrigins = [
+  "https://mern-auth-livid-seven.vercel.app",
+  "http://localhost:5173",
+];
 
-    // in this variable we can use all frontend url where we want to add this backend
-    const allowedOrigins = [
-    "https://mern-auth-livid-seven.vercel.app",
-    "http://localhost:5173"
-    ];
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 
-    app.use(express.json());
-    app.use(cookieParser());
-    app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-        } else {
-        callback(new Error("CORS Not Allowed"));
-        }
-    },
-    credentials: true
-    }));
+// for printing what will on Screen
+app.get("/", (req, res) => res.send("Api Working"));
 
+//api end point /api/auth is for every url and authRouter call function according to name
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
-    // for printing what will on Screen
-    app.get("/", (req, res) => res.send("Api Working"));
-
-    //api end point /api/auth is for every url and authRouter call function according to name 
-    app.use('/api/auth' , authRouter);
-    app.use('/api/user' , userRouter);
-
-    // It's for Starting Server
-    app.listen(port ,() => console.log(`server started on port : ${port}`));
+// It's for Starting Server
+app.listen(port, () => console.log(`server started on port : ${port}`));
