2840fc47d3027cc2736260a65d96e37ed2d287f3
Coder <altamasma064@gmail.com>
2025-11-26 21:43:24 +0530

    all COrsFix

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const allowedOrigins = [
   "http://localhost:5173",
 ];
 
-app.set("trust proxy", 1);
+// app.set("trust proxy", 1);
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({origin:allowedOrigins,credentials:true}));
